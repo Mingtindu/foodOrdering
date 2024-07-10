97fe4554a4d6cd45be33256341d2c34cb938277a
Mingtindu <mingmartu@gmail.com>
2024-07-10 07:31:08 +0545

    feat: Navbar created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,19 @@
 import Navbar from './components/Navbar/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 function App() {
   return (
     <>
     <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/order' element={<PlaceOrder/>}/>
+
+
+    </Routes>
     </>
   )
 }
