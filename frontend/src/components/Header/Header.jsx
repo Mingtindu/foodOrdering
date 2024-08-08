ddd90d73bfb27338ef7c45b5969d5ed8d0746408
Mingtindu <mingmartu@gmail.com>
2024-08-08 12:24:00 +0545

    feat: home page code

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+export const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-content">
+            <h2>Order you likely food here</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, molestias!</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
